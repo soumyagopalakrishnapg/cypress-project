66b5e799d6a81962da7a700340254e758482ca47
Gopalakrishna <SGOPALAKRISHNA@beckman.com>
2026-03-26 08:34:50 +0530

    trigger pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
