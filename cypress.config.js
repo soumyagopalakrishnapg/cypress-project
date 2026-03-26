6c469bdc57c2fc1b079cfcecca11d4b27ed352d3
Gopalakrishna <SGOPALAKRISHNA@beckman.com>
2026-03-26 09:15:15 +0530

    Added projectId for Cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "mochawesome",
+  projectId: "yffsfq",   // ✅ comma added
 
+  reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/reports",
     overwrite: false,
@@ -11,8 +12,6 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
