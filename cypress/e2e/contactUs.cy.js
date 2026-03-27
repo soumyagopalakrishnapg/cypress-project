5178030491e0c8ac307ae9849becbe9cc86b9ec5
Gopalakrishna <SGOPALAKRISHNA@beckman.com>
2026-03-27 17:34:12 +0530

    Added Contact Us form test

--- cypress/e2e/contactUs.cy.js
+++ cypress/e2e/contactUs.cy.js
@@ -5,12 +5,12 @@ describe('Contact Us Form Test', () => {
     // Visit page
     
    cy.visit('https://uat-dxweb104.beccloud.com/en/support/contact-us', {
-  timeout: 120000,
+  timeout: 220000,
   failOnStatusCode: false
 })
 
     // Verify page
-    cy.contains('Contact Us', { timeout: 60000 }).should('be.visible')
+    cy.contains('Contact Us', { timeout: 220000 }).should('be.visible')
 
     // Select dropdowns (adjust text if needed)
     cy.contains('Select country/region').parent().find('select').select(1)
